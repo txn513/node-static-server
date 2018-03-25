9368dbfb7402e733aa8194b10b4ffd24f19e3ad8
Lance Tian <xtian513@gmail.com>
2018-03-25 19:28:07 +0800

    update server

--- server.js
+++ server.js
@@ -8,28 +8,32 @@ http.createServer(function(req,res){
 	// console.log('the server is running');
 	let urlObj = url.parse(req.url);
 	let pathname = urlObj.pathname;
+
+    if(pathname.indexOf('.') == -1){
+        pathname = '/index.html';
+    }
+
 	let ext = path.extname(pathname);
-	console.log(pathname);
-
-	if(pathname == '/'){
-		pathname = '/index.html';
-		ext = '.html';
-	}
+	// console.log(pathname);
 
 
-	readMIME(function(mime){
-		fs.readFile('./static'+pathname,function(err,data){
-			if(err){
-				res.writeHead(404,{'content-type': 'text/html; charset=utf8'});
-				res.end('404');
-				return;
-			}
-			console.log(data);
-			res.writeHead(200,{'content-type': mime[ext] +'; charset=utf8'});
-			res.end(data);
 
-		});
-	});
+    fs.readFile('./static'+pathname,function(err,data){
+        if(err){
+            res.writeHead(404,{'content-type': 'text/html; charset=utf8'});
+            res.end('404');
+            return;
+        }
+
+        readMIME(function(mime){
+            res.writeHead(200,{'content-type': mime[ext] +'; charset=utf8'});
+            res.end(data);
+        });
+        // console.log(data);
+
+
+    });
+
 	
 }).listen(3000);
 
